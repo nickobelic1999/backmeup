1d1e1b03faa9c811d4fc4e33f25db0a55d716ce2
Nicholas White <nickobelic1999@gmail.com>
2022-08-16 18:04:34 +1000

    Authenticated and parsed json file.

--- cmake-build-debug/external_libs/pugixml/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nickobelic1999/CLionProjects/backmeup/external_libs/pugixml
-# Build directory: /home/nickobelic1999/CLionProjects/backmeup/cmake-build-debug/external_libs/pugixml
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
